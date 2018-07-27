11941c4a1e8abd77f77b6640e8c651087c47b498
pjaselin <pjaselin@users.noreply.github.com>
2018-07-27 12:23:48 -0400

    added comments

--- lammps_helper.py
+++ lammps_helper.py
@@ -1,9 +1,21 @@
 import numpy as np
 
 def c_to_np(carray_input):
-  result = np.array(np.zeros((50,2)))
-  for i in range(0,50):
-    result[i,0] = carray_input[i][0]
-    result[i,1] = carray_input[i][1]
-  
-  return(result)
+    '''
+    Description: Converts a LAMMPS C array to a NumPy array (only for reading RDF data)
+    Input:
+        carray_input: lmp variable with C array data
+    
+    Output:
+        result: NumpPy array
+    '''
+    # create an empty NumPy array
+    result = np.array(np.zeros((50,2)))
+    
+    # loop through C array and enter into NumPy array
+    for i in range(0,50):
+        result[i,0] = carray_input[i][0]
+        result[i,1] = carray_input[i][1]
+    
+    # return NumPy array
+    return(result)
